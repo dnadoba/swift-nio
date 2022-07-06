e726d796a78cd0bd32d165223e12976d86c89851
David Nadoba <d_nadoba@apple.com>
2022-07-06 13:20:55 +0200

    prototype of `CurrentEventLoop` and `EventLoopFutureWithThreadSafeCallbacks`
    Closures are **not** marked `@Sendable` for these types and they are not `Sendable` and therefore can safly capture local mutable variables. Just and experiment though

---
+++ Sources/NIOCore/EventLoopPromiseOnCurrentEventLoop.swift
@@ -0,0 +1,44 @@
+public struct EventLoopPromiseOnCurrentEventLoop<Value> {
+    @usableFromInline var wrapped: EventLoopPromise<Value>
+    
+    @inlinable public var futureResult: EventLoopFutureOnCurrentEventLoop<Value> {
+        wrapped.futureResult.iKnowIAmOnTheEventLoopOfThisFuture()
+    }
+    
+    @inlinable
+    init(_ promise: EventLoopPromise<Value>) {
+        self.wrapped = promise
+    }
+}
+
+#if swift(>=5.6)
+@available(*, unavailable)
+extension EventLoopPromiseOnCurrentEventLoop: Sendable {}
+#endif
+
+extension EventLoopPromise {
+    @inlinable public func iKnowIAmOnTheEventLoopOfThisPromise(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> EventLoopPromiseOnCurrentEventLoop<Value> {
+        self.futureResult.eventLoop.preconditionInEventLoop(file: file, line: line)
+        return .init(self)
+    }
+}
+
+extension EventLoopPromiseOnCurrentEventLoop {
+    @inlinable
+    public func succeed(_ value: Value) {
+        self.wrapped.succeed(value)
+    }
+
+    @inlinable
+    public func fail(_ error: Error) {
+        self.wrapped.fail(error)
+    }
+    
+    @inlinable
+    public func completeWith(_ result: Result<Value, Error>) {
+        self.wrapped.completeWith(result)
+    }
+}
